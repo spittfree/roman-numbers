893d5baf4820d90bd5fdb36be1237c6875705c3d
Carles <spittfree@gmail.com>
2015-08-29 13:54:14 +0200

    class to implement the interface

---
+++ app/RomanNumbersConverter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace RomanNumbers;
+
+
+class RomanNumbersConverter implements RomanNumeralGenerator {
+
+    /**
+     * Convert from integer to roman
+     * @param int $value
+     * @return string
+     */
+    public function generate($value)
+    {
+        // TODO: Implement generate() method.
+    }
+
+    /**
+     * @param string $value
+     * @return int
+     */
+    public function parse($value)
+    {
+        // TODO: Implement parse() method.
+    }
+}
