8b3c3e1c07f38d0363cdd167a517930c0955ab8c
Carles <spittfree@gmail.com>
2015-08-29 20:50:29 +0200

    refactor and cleanup

--- app/RomanNumbers/Logic/Roman/Logic.php
+++ app/RomanNumbers/Logic/Roman/Logic.php
@@ -31,21 +31,52 @@ class Logic
      */
     public function convert($input)
     {
+        $symbols = $this->prepareArrayOfSymbols($input);
+        return $this->processSymbols($symbols);
+    }
+
+    /**
+     * This functions decides if the Symbol should be added or subtracted
+     *
+     * @param int $lastSymbolValue
+     * @param string $symbol
+     * @return bool
+     * @throws InvalidValueException
+     */
+    private function shouldBeAdded($lastSymbolValue, $symbol)
+    {
+        return $lastSymbolValue <= $this->symbolsMapper->convert($symbol);
+    }
+
+    /**
+     * @param string $input
+     * @return array
+     */
+    private function prepareArrayOfSymbols($input)
+    {
         $symbols = str_split($input);
         $symbols = array_reverse($symbols);
+        return $symbols;
+    }
 
+    /**
+     * @param array $symbols
+     * @return int
+     * @throws InvalidValueException
+     */
+    private function processSymbols($symbols)
+    {
         $result = 0;
         $lastSymbolValue = 0;
 
         foreach ($symbols as $symbol) {
-            if ($lastSymbolValue <= $this->symbolsMapper->convert($symbol)) {
-                $result = $result + $this->symbolsMapper->convert($symbol);
+            if ($this->shouldBeAdded($lastSymbolValue, $symbol)) {
+                $result += $this->symbolsMapper->convert($symbol);
             } else {
-                $result = $result - $this->symbolsMapper->convert($symbol);
+                $result -= $this->symbolsMapper->convert($symbol);
             }
             $lastSymbolValue = $this->symbolsMapper->convert($symbol);
         }
-
         return $result;
     }
 }
