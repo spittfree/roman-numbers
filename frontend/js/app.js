8f807a2f7a258ba428745c1b1bdbee2b7032e98b
Carles <spittfree@gmail.com>
2015-09-03 20:07:58 +0200

    added angular, jquery and basic workflow

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,52 @@
-angular.module('romanDecimalApp', [])
-    .controller('toRomanController', function($scope, $http) {
+angular.module('numbersApp', [])
+    .controller('converter', function($scope, $http) {
+
+        $scope.input = '';
+        $scope.errorMessage = '';
+        $scope.result = '';
 
         $scope.convert = function() {
-            alert('hello');
+
+            if ($scope.__validate()) {
+                //URL to rest api to convert to roman
+                $http.post('/', {msg: $scope.input}).
+                    then(function (response) {
+                        //SUCCESS
+                        $scope.result = '';
+                    }, function (response) {
+                        //ERROR
+                        $scope.displayErrorMessage();
+                        $scope.hideSuccessMessage();
+                        //display error API message
+                        $scope.errorMessage = 'Error in the API';
+                    });
+            } else {
+                $scope.displayErrorMessage();
+                $scope.hideSuccessMessage();
+                $scope.errorMessage = 'Input is not valid';
+            }
+
         };
-    });
+
+        $scope.__validate = function() {
+           //validate data before call API
+            return false;
+        };
+
+        $scope.displayErrorMessage = function() {
+            $('#failure-box').removeClass('hidden');
+        };
+
+        $scope.displaySuccessMessage = function() {
+            $('#success-box').removeClass('hidden');
+        };
+
+        $scope.hideErrorMessage = function(){
+            $('#failure-box').addClass('hidden');
+        };
+
+        $scope.hideSuccessMessage = function(){
+            $('#success-box').addClass('hidden');
+        };
+
+    })
